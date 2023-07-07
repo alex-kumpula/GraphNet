4372fa88659ecd5b439015859148865c34c5bd38
alex-kumpula <alex.kumpula01@gmail.com>
2023-07-07 16:25:27 -0600

    Init

---
+++ GraphNet/IGraph.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Spundio.GraphNet
+{
+    public interface IGraph<TVertexKey, TVertexValue, TEdgeValue>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <value></value>
+        IEnumerable<TVertexKey> Keys { get; }
+        IEnumerable<TVertexValue> Values { get; }
+        IEnumerable<KeyValuePair<TVertexKey, TVertexValue>> Vertices { get; }
+
+        IEnumerator<KeyValuePair<TVertexKey, TVertexValue>> GetEnumerator();
+
+        void AddVertex(TVertexKey vertexKey, TVertexValue vertexValue);
+        void AddEdge(TVertexKey vertexKeyFrom, TVertexKey vertexKeyTo, TEdgeValue edgeValue);
+        void RemoveVertex(TVertexKey vertexKey);
+        void RemoveEdge(TVertexKey vertexKeyFrom, TVertexKey vertexKeyTo);
+        
+        bool VertexExists(TVertexKey vertexKey);
+        bool EdgeExists(TVertexKey vertexKeyFrom, TVertexKey vertexKeyTo);
+
+        TVertexValue GetVertexValue(TVertexKey vertexKey);
+    }
+
+
+}
---
+++ GraphNet/IVertexView.cs
@@ -0,0 +1,10 @@
+namespace Spundio.GraphNet
+{
+    public interface IVertexView<TVertexKey, TVertexValue, TEdgeValue>
+    {
+        TVertexKey Key { get; }
+        TVertexValue Value { get; set; }
+    }
+
+
+}
